8d9def81e70ed4d6522777061c9c47bfa386a616
Michael Adu-Gyamfi <119356822+mikkayadu@users.noreply.github.com>
2024-01-13 22:54:14 +0000

    Added solution for Find All Duplicates in an Array

---
+++ Cyclic Sort/find-all-duplicates-in-an-array.py
@@ -0,0 +1,16 @@
+class Solution:
+    def findDuplicates(self, nums: List[int]) -> List[int]:
+        i = 0
+        distinct = len(set(nums))
+
+        while i < len(nums):
+            if nums[i] != nums[nums[i] -1]:
+                nums[nums[i]-1], nums[i]  = nums[i], nums[nums[i] -1]
+            else:
+                i+=1
+        ans = [] 
+        for i in range(len(nums)):
+            if nums[i] != i+1:
+                ans.append(nums[i])        
+
+        return ans        
